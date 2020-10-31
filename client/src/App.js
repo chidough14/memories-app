7ec768e766a19bf60a83cc23e21eee0319b391b9
Chidozie <chidough14@gmail.com>
2020-10-31 16:45:35 +0100

    Frontend folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 
 
-import './App.css';
+import {Container, AppBar, Typography, Grid, Grow} from '@material-ui/core'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles()
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">Memories</Typography>
+          <img src={memories} height="60" alt="memories" />
+        </AppBar>
+
+        <Grow in>
+          <Container>
+              <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                <Grid item xs={12} sm={7}>
+                  <Posts />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                  <Form />
+                </Grid>
+              </Grid>
+          </Container>
+        </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+
+
+const Form = () => {
+    return (
+        <div>
+            <h1>form</h1>
+        </div>
+    )
+}
+
+export default Form
